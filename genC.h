8a46afe40ba3962532da5cfbd224297027b6c190
icode4fun <a.dunbar@ymail.com>
2014-03-05 16:28:19 -0600

    Update genC.h

--- genC.h
+++ genC.h
@@ -1,2 +1,11 @@
 template<typename T>
-bool isPresent(T *list, T key, int size);
+bool isPresent(T *list, T key, int size){
+    
+    for(int i = 0; i < size; i++)
+    {
+        if(list[i] == key)
+            return true;
+    }
+    
+    return false;
+}
